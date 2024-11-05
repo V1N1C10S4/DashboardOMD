01330075a59efecf7a442b83d030c621d9356d28
V1N1C10S4 <santoyocuevas.vinicio@gmail.com>
2024-11-04 19:51:03 -0600

    correcciones

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,27 +13,67 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-# Logotipo con tamaño personalizado
-st.image("images/OMD2.png")
-
 # Configuración de las páginas
-PAGES = {
-    "Introduction": "Background/intro.py",
-    "Facts": "Background/facts.py",
-    "Interactive charts": "Visualization/visualization.py",
-    "Sentiment analysis": "ml/ml_analysis.py",
-    "Statistics": "EDA/eda.py",
-    "About Us": "Background/about.py",
-    "Copyright": "Background/example.py"
+intro = st.Page(
+    "Background/intro.py",
+    title="Introduction",
+    icon=":material/help:",
+)
+
+facts = st.Page(
+    "Background/facts.py",
+    title="Facts",
+    icon=":material/help:",
+)
+
+visualization = st.Page(
+    "Visualization/visualization.py",
+    title="Interactive charts",
+    icon=":material/help:",
+)
+
+ml = st.Page(
+    "ml/ml_analysis.py",
+    title="Sentiment analysis",
+    icon=":material/healing:",
+)
+
+eda = st.Page(
+    "EDA/eda.py",
+    title="Statistics",
+    icon=":material/person_add:",
+)
+
+about = st.Page(
+    "Background/about.py",
+    title="About Us",
+    icon=":material/person_add:",
+)
+
+cr = st.Page(         
+    "Background/example.py",
+    title="Copyright",
+    icon=":material/person_add:",
+)
+
+intro_pages = [intro, facts]
+visualization_pages = [visualization]
+ml_pages = [ml]
+eda_pages = [eda]
+about_pages = [about, cr]
+
+# Logotipo con tamaño personalizado
+st.logo("images/OMD2.png", icon_image="images/OMD2.png")
+
+# Diccionario de navegación
+page_dict = {
+    "Introduction": intro_pages,
+    "Data Analysis": eda_pages,
+    "Visualization": visualization_pages,
+    "Prediction": ml_pages,
+    "About us": about_pages,
 }
 
-# Selector de página en la barra lateral
-st.sidebar.title("Navigation")
-selection = st.sidebar.radio("Go to", list(PAGES.keys()))
-
-# Cargar el contenido de la página seleccionada
-page = PAGES[selection]
-
-with open(page, "r") as f:
-    code = f.read()
-    exec(code)
+# Configuración de navegación
+pg = st.navigation(page_dict)
+pg.run()
