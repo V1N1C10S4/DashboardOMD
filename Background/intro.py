4e9425867040b8d288bec569c2a85cc252a81fc1
V1N1C10S4 <santoyocuevas.vinicio@gmail.com>
2024-11-25 11:58:59 -0600

    Intro corrections

--- Background/intro.py
+++ Background/intro.py
@@ -6,10 +6,10 @@ image_map = Image.open("images/mapa_mexico.png")  # Imagen del mapa de México
 image_combined = Image.open("images/candidatos.png")  # Imagen combinada de candidatos y boletas
 
 # Diseño en columnas
-col1, col2 = st.columns([2, 1])  # Ajustar proporciones para dar más espacio al texto
+col1, col2 = st.columns([1.5, 2])  # Ajustar proporciones para reducir el ancho de la columna izquierda
 
 with col1:
-    # Título e introducción
+    # Título e introducción con salto de línea entre párrafos
     st.markdown(
         """
         <div style='text-align: left; padding-top: 20px;'>
@@ -18,6 +18,9 @@ with col1:
                 En un año decisivo para México, las elecciones presidenciales de 2024 también han tenido un impacto monumental en el ecosistema digital, 
                 particularmente en la plataforma X (anteriormente Twitter). Este proyecto de análisis de datos profundiza en las dinámicas de interacción 
                 de la comunidad en X, desentrañando patrones, anomalías y comportamientos influyentes detrás de los posts relacionados con las elecciones.
+            </p>
+            <br>
+            <p style='font-size: 1.1em; color: #FAFAFA; text-align: justify;'>
                 En su esencia, este dashboard no solo se centra en las elecciones como evento, sino en cómo las voces individuales y colectivas moldean 
                 la narrativa pública en un acontecimiento de tal magnitud. Al identificar anomalías y comportamientos inusuales, buscamos extraer insights 
                 clave que permitan comprender mejor a la sociedad en procesos históricos como este, con el potencial de transformar cómo entendemos la 
