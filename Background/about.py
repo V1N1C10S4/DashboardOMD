83a8e224257357b1c340336582d9b6556105287f
V1N1C10S4 <santoyocuevas.vinicio@gmail.com>
2024-11-26 04:05:19 -0600

    About page modifications

--- Background/about.py
+++ Background/about.py
@@ -6,34 +6,36 @@ def get_base64_image(image_path):
     with open(image_path, "rb") as img_file:
         return base64.b64encode(img_file.read()).decode()
 
-# Configurar la página "Sobre Nosotros"
+# Configuración del cuadro negro con texto
 st.markdown("""
 <div style="background-color: white; padding: 20px; border-radius: 10px; text-align: center; width: 90%; margin: auto;">
-    <h1 style='text-align: center; color: black;'>¿Quiénes somos?</h1>
-    <p style="color: black; font-size: 18px; margin: 20px;">
-        Somos un grupo de estudiantes interdisciplinario del Tecnológico de Monterrey trabajando juntos para aportar de nuestros conocimientos y descubrimientos al resto de la comunidad universitaria y principalmente a México.
-    </p>
-    <div style="display: flex; justify-content: space-around; margin-top: 20px;">
-        <div>
-            <img src="data:image/png;base64,{}" style="border-radius: 50%; width: 150px;">
-            <p style="color: black; font-size: 14px; font-weight: bold;">Vinicio Santoyo Cuevas<br>ITD</p>
-        </div>
-        <div>
-            <img src="data:image/png;base64,{}" style="border-radius: 50%; width: 150px;">
-            <p style="color: black; font-size: 14px; font-weight: bold;">Manuel Medina Juárez<br>ITD</p>
-        </div>
-        <div>
-            <img src="data:image/png;base64,{}" style="border-radius: 50%; width: 150px;">
-            <p style="color: black; font-size: 14px; font-weight: bold;">Gabriel Josué Tabango Espinoza<br>ITD</p>
-        </div>
+    <div style="background-color: black; padding: 20px; border-radius: 10px; margin-bottom: 20px;">
+        <h1 style='text-align: center; color: white;'>¿Quiénes somos?</h1>
+        <p style="color: white; font-size: 18px; margin: 20px;">
+            Somos un grupo de estudiantes interdisciplinario del Tecnológico de Monterrey trabajando juntos para aportar de nuestros conocimientos y descubrimientos al resto de la comunidad universitaria y principalmente a México.
+        </p>
     </div>
     <div style="display: flex; justify-content: space-around; margin-top: 20px;">
         <div>
-            <img src="data:image/png;base64,{}" style="border-radius: 50%; width: 150px;">
+            <img src="images/vinicio_santoyo.png" style="border-radius: 50%; width: 150px;">
+            <p style="color: black; font-size: 14px; font-weight: bold;">Vinicio Santoyo Cuevas<br>ITD</p>
+        </div>
+        <div>
+            <img src="images/manuel_medina.png" style="border-radius: 50%; width: 150px;">
+            <p style="color: black; font-size: 14px; font-weight: bold;">Manuel Medina Juárez<br>ITD</p>
+        </div>
+        <div>
+            <img src="images/gabriel_tabango.png" style="border-radius: 50%; width: 150px;">
+            <p style="color: black; font-size: 14px; font-weight: bold;">Gabriel Josué Tabango Espinoza<br>ITD</p>
+        </div>
+    </div>
+    <div style="display: flex; justify-content: space-evenly; margin-top: 0px;"> <!-- Espacio reducido entre filas -->
+        <div>
+            <img src="images/andres_martinez.png" style="border-radius: 50%; width: 150px;">
             <p style="color: black; font-size: 14px; font-weight: bold;">Andrés Emiliano Martínez Fuentes<br>ITD</p>
         </div>
         <div>
-            <img src="data:image/png;base64,{}" style="border-radius: 50%; width: 150px;">
+            <img src="images/luis_gonzalez.png" style="border-radius: 50%; width: 150px;">
             <p style="color: black; font-size: 14px; font-weight: bold;">Luis Fernando González Cortés<br>ITD</p>
         </div>
     </div>
