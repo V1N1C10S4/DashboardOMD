aa650264b1497b45577cee126ebbda5cb04fc965
V1N1C10S4 <santoyocuevas.vinicio@gmail.com>
2024-11-25 18:27:02 -0600

    Palette changes

--- Clustering/clustering.py
+++ Clustering/clustering.py
@@ -64,7 +64,7 @@ fig = px.scatter(
     color=pca_df['cluster'].astype(str),  # Convertir cluster a cadena para una leyenda categórica
     title="Basado en PCA y KMeans",
     labels={'color': 'Clusters', 'PCA1': 'Componente principal 1', 'PCA2': 'Componente principal 2'},  # Cambiar el título del filtro de color
-    color_discrete_sequence=px.colors.sequential.Viridis  # Colores vibrantes
+    color_discrete_sequence=px.colors.diverging.Spectral  # Colores vibrantes
 )
 
 # Agregar líneas de referencia en los ejes
