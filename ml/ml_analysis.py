5de4757a83c81d914809fe81297e084e10ccd7cd
V1N1C10S4 <santoyocuevas.vinicio@gmail.com>
2024-11-26 12:22:15 -0600

    Second sentiment plot

--- ml/ml_analysis.py
+++ ml/ml_analysis.py
@@ -47,7 +47,7 @@ fig.add_trace(go.Scatter(
 fig.update_layout(
     title='Tendencia del Sentimiento Promedio por Semana',
     xaxis=dict(
-        title='Semana',
+        title='Mes',
         titlefont=dict(size=14, weight='bold'),
         tickformat='%b %d',  # Mostrar el mes abreviado y el día
         tickangle=45,
@@ -69,4 +69,77 @@ fig.update_layout(
 )
 
 # Mostrar la gráfica en Streamlit
+st.plotly_chart(fig, use_container_width=True)
+
+# Añadir un espacio entre la gráfica de clustering y las gráficas de análisis de clusters
+st.markdown("<div style='margin: 40px 0;'></div>", unsafe_allow_html=True)
+
+top_users_df = df[df['top_user_indicator'] == 1]
+
+# Agrupar por sentimiento y sumar el número de interacciones
+interaction_sum = top_users_df.groupby('predicted_sentiment')['num_interaction'].sum().reset_index()
+print(interaction_sum)
+
+# Crear la gráfica de barras
+fig = go.Figure()
+
+# Colores asignados
+bar_colors = ['red', 'gray', 'green']
+
+# Añadir las barras a la gráfica
+fig.add_trace(go.Bar(
+    x=interaction_sum['predicted_sentiment'],  # Eje X: Valores de sentimiento
+    y=interaction_sum['num_interaction'],  # Eje Y: Total de interacciones
+    marker_color=bar_colors,  # Colores personalizados
+    name='Interacciones'
+))
+
+# Configurar el diseño de la gráfica
+fig.update_layout(
+    title={
+        'text': 'Total de Interacciones por Valor de Sentimiento para Usuarios Destacados',
+        'y': 0.9,
+        'x': 0.5,
+        'xanchor': 'center',
+        'yanchor': 'top',
+        'font': {'size': 18, 'color': 'black'}
+    },
+    xaxis=dict(
+        title='Valor de Sentimiento',
+        tickvals=[0, 1, 2],  # Valores en el eje X
+        ticktext=['0', '1', '2'],  # Etiquetas en el eje X
+        title_font=dict(size=14, color='black'),
+        tickfont=dict(size=12, color='gray')
+    ),
+    yaxis=dict(
+        title='Número Total de Interacciones',
+        tickvals=[10_000_000, 20_000_000, 30_000_000, 40_000_000, 50_000_000],  # Valores en millones
+        ticktext=['10M', '20M', '30M', '40M', '50M'],  # Etiquetas con sufijo 'M'
+        range=[10_000_000, 50_000_000],  # Rango del eje Y
+        title_font=dict(size=14, color='black'),
+        tickfont=dict(size=12, color='gray'),
+        gridcolor='lightgray',
+        gridwidth=0.5
+    ),
+    template='simple_white',
+    plot_bgcolor='rgba(0, 0, 0, 0)',  # Fondo transparente
+    showlegend=False  # Ocultar leyenda
+)
+
+# Insertar logo en la esquina superior derecha
+fig.add_layout_image(
+    dict(
+        x=1.1,
+        y=1.15,
+        xref="paper",
+        yref="paper",
+        sizex=0.15,
+        sizey=0.15,
+        xanchor="right",
+        yanchor="top"
+    )
+)
+
+# Mostrar la gráfica en Streamlit
+import streamlit as st
 st.plotly_chart(fig, use_container_width=True)
